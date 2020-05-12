108900acf5ad5769e72e9e37d77d9af6caaf3e1c
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-05-12 16:25:53 +1000

    -completed neural network implemtation \n -completed ex3

--- machine-learning-ex3/machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/machine-learning-ex3/ex3/predict.m
@@ -20,8 +20,13 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-
-
+X = [ones(m, 1) X];
+C = sigmoid(X * Theta1');
+n = size(C,1);
+C = [ones(n, 1) C];
+D = sigmoid(C * Theta2');
+[val,p] = max(D,[],2);
+% D = sigmoid(
 
 
 
